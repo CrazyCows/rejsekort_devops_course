2b728ffadc59517fc1e3f1a9a82b610415c99437
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2024-09-19 15:31:16 +0200

    remove unused imports

--- app/src/main/java/devops/rejsekort/ui/views/CheckInView.kt
+++ app/src/main/java/devops/rejsekort/ui/views/CheckInView.kt
@@ -1,20 +1,14 @@
 package devops.rejsekort.ui.views
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
