823ad9b66279c0b068d76bdaf7206b2859edd1f7
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2024-09-19 15:40:28 +0200

    chanced to data class

--- app/src/main/java/devops/rejsekort/data/UserData.kt
+++ app/src/main/java/devops/rejsekort/data/UserData.kt
@@ -1,16 +1,7 @@
 package devops.rejsekort.data
 
-class UserData {
-    val firstName: String
-    val lastName: String
-    val isCheckedIn: Boolean
-
-
-    constructor(firstName: String, lastName: String, isCheckedIn: Boolean) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.isCheckedIn = isCheckedIn
-    }
-
-
-}
+data class UserData (
+    val firstName: String = "",
+    val lastName: String = "",
+    val isCheckedIn: Boolean = false,
+)
