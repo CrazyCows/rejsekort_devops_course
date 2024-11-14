c7c4ddb5cf34e7306405192810a6e6e1a38764f1
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2024-11-14 14:58:58 +0100

    version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "devops.rejsekort"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "devops.rejsekort"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
